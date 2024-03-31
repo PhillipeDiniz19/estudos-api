a1c5f8f513a156153c710f16b72d2d14a2d1fb60
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-03-31 20:58:19 -0300

    subindo teste home

--- src/template/Home/Home.spec.jsx
+++ src/template/Home/Home.spec.jsx
@@ -1,7 +1,49 @@
+import { rest } from 'msw';
+import { setupServer } from 'msq/node';
+
+const { render, screen, waitForElementToBeRemoved } = require('@testing-library/react');
+import Home from './Home';
+
+const handlers = [
+  rest.get('*jsonplaceholder.typicode.com*', async (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          userId: 1,
+          id: 1,
+          title: 'title1',
+          body: 'body1',
+          url: 'img1.jpg',
+        },
+        {
+          userId: 2,
+          id: 2,
+          title: 'title2',
+          body: 'body2',
+          url: 'img1.jpg',
+        },
+        {
+          userId: 3,
+          id: 3,
+          title: 'title3',
+          body: 'body3',
+          url: 'img3.jpg',
+        },
+      ]),
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
 describe('<Home/>', () => {
   // dentro do describe eu vou ter os teste do componente que é o componente home e pode trocar para outros components.
-  it('dummy one', () => {
-    expect(1).toBe(1); // espero que "expect" seja igual a "toBe"
+  it('should render search, posts, and load more', async () => {
+    render(<Home />);
+    const noMorePosts = screen.getByText('Não existem posts =(');
+
+    await waitForElementToBeRemoved(noMorePosts);
+    screen.debug();
   });
 });
 
